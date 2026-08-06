fd69f1c118708463f7223804fe9ad5a665f0874d
yk-graph <yokokura.tatsuya@gmail.com>
2026-08-05 20:51:57 -0700

    add: custom hooks

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+
+import { ProductsContext } from '../contexts/productsContext'
+
+export function useProducts() {
+  const context = useContext(ProductsContext)
+
+  if (!context) {
+    throw new Error('useProducts must be used within ProductsProvider')
+  }
+
+  return context
+}
