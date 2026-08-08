ab7018592ef999e0c3f08f03efabfbb36cd642d5
yk-graph <yokokura.tatsuya@gmail.com>
2026-08-08 04:44:53 -0700

    add: shalow grad black style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,13 @@ import LocationMarquee from '../components/home/LocationMarquee'
 
 export default function Home() {
   return (
-    <>
+    <div className="relative">
       <Hero />
       <LocationMarquee />
       <AboutSection />
-    </>
+
+      <div className="pointer-events-none absolute inset-y-0 left-0 w-24 bg-linear-to-r from-ink to-transparent sp:w-40" />
+      <div className="pointer-events-none absolute inset-y-0 right-0 w-24 bg-linear-to-l from-ink to-transparent sp:w-40" />
+    </div>
   )
 }
